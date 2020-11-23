f2db7905be3eebfcf7d8227ac58fe23e15ab2c48
Guyra Anderson <56006730+iguyra@users.noreply.github.com>
2020-11-23 11:10:55 -0800

    Update Heading.js

--- components/Heading.js
+++ components/Heading.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import axios from "axios";
-import { getFrontUser } from "../../backend/fauthContoller";
+// import { getFrontUser } from "../../backend/fauthContoller";
 import CartIcon from "./cart/CartIcon";
 
 class Heading extends React.Component {
