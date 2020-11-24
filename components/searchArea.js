476e4be4c1b02b137ea2af495a0f42132e9e0453
Guyra Anderson <burnx16@gmail.com>
2020-11-24 12:23:55 -0800

    changes

--- components/searchArea.js
+++ components/searchArea.js
@@ -7,7 +7,8 @@ const SearchArea = () => {
         <section>
             <div className="search">
                 <div className="search__back"><i class="fas fa-long-arrow-alt-left"></i></div>
-                <input className="search__input" type="text" placeholder="search products"/>
+                <input className="search__input" type="text" placeholder="search products" autofocus />
+                
                 <div className="search__button">search</div>
             </div>
         </section>
