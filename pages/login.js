e0411ccfce2dfc337068fd0689a6c908ce19e65a
Guyra Anderson <burnx16@gmail.com>
2020-12-11 10:12:15 -0800

    changes

--- pages/login.js
+++ pages/login.js
@@ -53,7 +53,7 @@ class login extends React.Component {
     return (
       <Layout>
         <section className="siginin" id="signin">
-          {error ? <p className="error">{ errorMsg}</p>: ""}
+         <p className="error">{ errorMsg}</p>
           <Link href="/">
             <a className="siginin__sitename" id="sitename">
               iguyra <span>&larr;</span>
