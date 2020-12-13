795fdcfcfd0d339b32751bfcae32fd3cb296c03b
Guyra Anderson <burnx16@gmail.com>
2020-12-12 16:23:56 -0800

    changes

--- functions/URLbaseAPI.js
+++ functions/URLbaseAPI.js
@@ -1,9 +1,9 @@
 let BASE_URL = "http://localhost:3080"
 
-    if (process.env === "development") {
+    if (process.env.NODE_ENV === "development") {
         BASE_URL = BASE_URL
 }
-if (process.env === "production") {
+if (process.env.NODE.ENV === "production") {
         BASE_URL = "https://iguyra.herokuapp.com"
     }
 
