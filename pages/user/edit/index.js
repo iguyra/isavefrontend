e124e76588e34bb9826eaea3279f88e9b7577c08
Guyra Anderson <burnx16@gmail.com>
2021-01-11 15:27:07 -0800

    changes

--- pages/user/edit/index.js
+++ pages/user/edit/index.js
@@ -1,32 +1,33 @@
-import React from "react";
-import Link from "next/link";
-import axios from "axios";
-import Router from "next/router";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+import Router from 'next/router';
 
-import Header from "../../../components/Heading";
-import Layout from "../../../components/Layout";
-import { getFrontUser,getServerSideToken,getClientSideToken } from "../../../functions/fauthContoller";
-import URLbaseAPI from "../../../functions/URLbaseAPI"
+import Header from '../../../components/Heading';
+import Layout from '../../../components/Layout';
+import {
+  getFrontUser,
+  getServerSideToken,
+  getClientSideToken,
+} from '../../../functions/fauthContoller';
+import URLbaseAPI from '../../../functions/URLbaseAPI';
 
+const edit = ({ userr }) => {
+  const [inputField, setInputField] = useState({
+    firstname: '',
+    lastname: '',
+    phonenumber: '',
+    email: '',
+    gender: '',
+  });
 
-class edit extends React.Component {
-  state = {
-    user: {},
-    firstname: "",
-    lastname: "",
-    phonenumber: "",
-    email: "",
-    gender: "",
-    isUpdating: false,
-    isUpdated: false,
-    error : false
+  const [user, setUser] = useState({});
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [error, setError] = useState(false);
 
-  };
-
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-    
+  const handleChange = (event) => {
+    setInputField({ [event.target.name]: event.target.value });
   };
 
   // async componentDidMount() {
@@ -37,29 +38,33 @@ class edit extends React.Component {
   //   }
   //   console.log(data)
   // }
+  const { firstname, lastname, phonenumber } = inputField;
+  console.log(firstname);
 
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     try {
-      const {isUpdated ,firstname, lastname,phonenumber } = this.state;
-      this.setState({ isUpdating: true })
-      const token = localStorage.getItem("token")
+      setIsUpdating(true);
+      const token = localStorage.getItem('token');
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
-      const { data } = await axios.patch(`${URLbaseAPI}/api/users/updateUser`, { firstname, lastname, phonenumber },config);
+      const { data } = await axios.patch(
+        `${URLbaseAPI}/api/users/updateUser`,
+        { firstname, lastname, phonenumber },
+        config
+      );
 
-      this.setState({ user: data });
-      this.setState({isUpdating: false})
-      this.setState({ isUpdated: true })
-      Router.push("/user");
-
+      setUser(data);
+      setIsUpdating(false);
+      setIsUpdated(true);
+      Router.push('/user');
     } catch (error) {
-      console.log(error.response)
-      this.setState({isUpdating: false})
+      console.log(error.response);
+      setIsUpdating(false);
 
       // if (error.response === "undefined") {
       //    this.setState({ errorMsg: "something went wrong, try again later" })
@@ -71,85 +76,96 @@ class edit extends React.Component {
       // }
       // console.log("error.response",error.response)
       // this.setState({ error: true })
-      
-
-   }
+    }
   };
+  // const { user, firstname, lastname, phonenumber } = this.props;
 
-  render() {
-    const { user,firstname, lastname,phonenumber} = this.props
+  // const { isUpdated, isUpdating } = this.state;
 
-    const {isUpdated, isUpdating} = this.state
-    return (
-      <section className="useredit" id="signup">
-        <div className="secondaryheading">
-          <p className="secondaryheading__word">account details</p>
+  console.log(userr);
+  return (
+    <section className="useredit" id="signup">
+      <div className="secondaryheading">
+        <p className="secondaryheading__word">account details</p>
+      </div>
+      {isUpdated && <p className="error">user details updated</p>}
+      <form className="form" action="" onSubmit={handleSubmit}>
+        <div className="form__group">
+          <label className="form__label" htmlFor="firstname">
+            first name
+          </label>
+          <input
+            className="form__input"
+            name="firstname"
+            type="text"
+            onChange={handleChange}
+            defaultValue={userr.firstname}
+          />
         </div>
-        {isUpdated && <p className="error">user details updated</p>}
-        <form className="form" action="" onSubmit={this.handleSubmit}>
-          <div className="form__group">
-            <label className="form__label" htmlFor="firstname">
-              first name
-            </label>
-            <input className="form__input" name="firstname" type="text" onChange={this.handleChange} defaultValue={user.firstname} />  
-            
-          </div>
-          <div className="form__group">
-            <label className="form__label" htmlFor="lastname">
-              last name
-            </label>
-            <input className="form__input" type="text" name="lastname" defaultValue={ user.lastname} onChange={this.handleChange}/>
-          </div>
+        <div className="form__group">
+          <label className="form__label" htmlFor="lastname">
+            last name
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            name="lastname"
+            defaultValue={userr.lastname}
+            onChange={handleChange}
+          />
+        </div>
 
-       
+        <div className="form__group">
+          <label className="form__label" htmlFor="phonenumber">
+            phone number
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            name="phonenumber"
+            defaultValue={phonenumber ? userr.phonenumber : ''}
+            onChange={handleChange}
+          />
+        </div>
 
-          <div className="form__group">
-            <label className="form__label" htmlFor="phonenumber">
-              phone number
-            </label>
-            <input className="form__input" type="text" name="phonenumber" defaultValue={user.phonenumber ? user.phonenumber : ""} onChange={this.handleChange}/>
-          </div>
+        <div className="form__group">
+          <label className="form__label" htmlFor="gender">
+            gender
+          </label>
+          <select name="gender" id="" onChange={handleChange}>
+            <option value="male">male</option>
+            <option value="female">female</option>
+          </select>
+        </div>
 
-          <div className="form__group">
-            <label className="form__label" htmlFor="gender">
-              gender
-            </label>
-            <select name="gender" id=""onChange={this.handleChange}>
-              <option value="male">male</option>
-              <option value="female">female</option>
-            </select>
-          </div>
+        <button className="form__button">
+          {isUpdating ? 'updating' : 'save'}
+        </button>
+      </form>
+    </section>
+  );
+};
 
-          <button className="form__button">{ isUpdating ? "updating": "save"}</button>
-        </form>
-      </section>
-    );
-  }
-}
-
-
-edit.getInitialProps  = async(ctx) => {
+edit.getInitialProps = async (ctx) => {
   try {
-  const {req} = ctx
-  
-  let token = req ? getServerSideToken(req) : getClientSideToken()   
+    const { req } = ctx;
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+    let token = req ? getServerSideToken(req) : getClientSideToken();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     };
 
-  const { data } = await axios.get(`${URLbaseAPI}/api/users/cart`, config);
-  
-  return {
-        user: data.user  // will be passed to the page component as props
-      }
+    const { data } = await axios.get(`${URLbaseAPI}/api/users/cart`, config);
+    console.log(data, 'data');
+    return {
+      userr: data.user, // will be passed to the page component as props
+    };
   } catch (err) {
-    console.log("errorr",err)
- }
-}
-
-
+    console.log('errorr', err);
+  }
+};
 
 export default edit;
--- pages/user/updatepassword/index.js
+++ pages/user/updatepassword/index.js
@@ -1,20 +1,18 @@
-import React from "react";
-import Link from "next/link";
-import Router from "next/router";
-import axios from "axios";
+import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+import axios from 'axios';
 
-import URLbaseAPI from "../../../functions/URLbaseAPI"
-
+import URLbaseAPI from '../../../functions/URLbaseAPI';
 
 class updatepassword extends React.Component {
   state = {
     user: {},
-    currentPassword: "xxxxxxxx",
-    password: "xxxxxxxx",
-    passwordConfirm: "xxxxxxxx",
-    errorMsg: "",
-    isUpdating: false
-
+    currentPassword: 'xxxxxxxx',
+    password: 'xxxxxxxx',
+    passwordConfirm: 'xxxxxxxx',
+    errorMsg: '',
+    isUpdating: false,
   };
 
   handleChange = (event) => {
@@ -23,68 +21,98 @@ class updatepassword extends React.Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
-console.log("clicked")
+    console.log('clicked');
     try {
-    const token = localStorage.getItem("token");
-    this.setState({isUpdating: true})
+      const token = localStorage.getItem('token');
+      this.setState({ isUpdating: true });
 
-    console.log("componentmounted", token);
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
+      console.log('componentmounted', token);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
 
-    const { currentPassword,password,passwordConfirm } = this.state;
-    console.log(currentPassword,password,passwordConfirm)
-    const { data } = await axios.patch(`${URLbaseAPI}/api/users/updatePassword`, {currentPassword,password,passwordConfirm},config);
-   
-console.log("password updated")
+      const { currentPassword, password, passwordConfirm } = this.state;
+      console.log(currentPassword, password, passwordConfirm);
+      const { data } = await axios.patch(
+        `${URLbaseAPI}/api/users/updatePassword`,
+        { currentPassword, password, passwordConfirm },
+        config
+      );
+
+      console.log('password updated');
       this.setState({ data: data });
-      Router.push("/");
+      Router.push('/');
     } catch (error) {
-      console.log("error",error.response.data)
-      this.setState({errorMsg: error.response.data})
-      this.setState({ error: true })
-      this.setState({isUpdating: false})
-   }
+      console.log('error', error.response.data);
+      this.setState({ errorMsg: error.response.data });
+      this.setState({ error: true });
+      this.setState({ isUpdating: false });
+    }
   };
 
   render() {
-    const {isUpdating,error, errorMsg,currentPassword,password,passwordConfirm} = this.state
+    const {
+      isUpdating,
+      error,
+      errorMsg,
+      currentPassword,
+      password,
+      passwordConfirm,
+    } = this.state;
     return (
       <section className="useredit" id="signup">
         <div className="secondaryheading">
           <p className="secondaryheading__word">change password</p>
         </div>
-        {error ? <p className="error">{ errorMsg.message}</p>: ""}
+        {error ? <p className="error">{errorMsg.message}</p> : ''}
         <form className="form" action="" onSubmit={this.handleSubmit}>
           <div className="form__group">
             <label className="form__label" htmlFor="email">
               current password
             </label>
-            <input onChange={this.handleChange} className="form__input" type="password" name="currentPassword" value={currentPassword} />
+            <input
+              onChange={this.handleChange}
+              className="form__input"
+              type="password"
+              name="currentPassword"
+              value={currentPassword}
+            />
           </div>
           <div className="form__group">
             <label className="form__label" htmlFor="email">
               new password
             </label>
-            <input  onChange={this.handleChange}  className="form__input" type="password" name="password" value={password} />
+            <input
+              onChange={this.handleChange}
+              className="form__input"
+              type="password"
+              name="password"
+              value={password}
+            />
           </div>
 
           <div className="form__group">
             <label className="form__label" htmlFor="email">
               retype new password
             </label>
-            <input  onChange={this.handleChange} className="form__input" type="password" name="passwordConfirm" value={passwordConfirm} />
+            <input
+              onChange={this.handleChange}
+              className="form__input"
+              type="password"
+              name="passwordConfirm"
+              value={passwordConfirm}
+            />
           </div>
 
-          <button disabled={isUpdating} className="form__button">{ isUpdating ? "updating...": "save"}</button>
+          <button disabled={isUpdating} className="form__button">
+            {isUpdating ? 'updating...' : 'save'}
+          </button>
         </form>
       </section>
     );
   }
 }
-
 
 export default updatepassword;
