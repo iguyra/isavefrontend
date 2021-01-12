652df5abab93305d83552cfe063d187e2fac3eec
Guyra Anderson <burnx16@gmail.com>
2021-01-11 16:45:16 -0800

    changes

--- pages/user/edit/index.js
+++ pages/user/edit/index.js
@@ -5,6 +5,7 @@ import Router from 'next/router';
 
 import Header from '../../../components/Heading';
 import Layout from '../../../components/Layout';
+import Loader from '../../../components/Loader/Loader';
 import {
   getFrontUser,
   getServerSideToken,
@@ -25,19 +26,20 @@ const edit = ({ userr }) => {
   const [isUpdating, setIsUpdating] = useState(false);
   const [isUpdated, setIsUpdated] = useState(false);
   const [error, setError] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const handleChange = (event) => {
     setInputField({ [event.target.name]: event.target.value });
-  };
+    console.log({ [event.target.name]: event.target.value });
+    let value = event.target.value;
 
-  // async componentDidMount() {
-  //   const token = localStorage.getItem("token");
-  //   const data = await getFrontUser(token);
-  //   if (data) {
-  //     this.setState({ user: data.user });
-  //   }
-  //   console.log(data)
-  // }
+    if (value.length > 0) {
+      setIsDisabled(false);
+    }
+    console.log(isDisabled);
+  };
+  console.log(isDisabled);
+
   const { firstname, lastname, phonenumber } = inputField;
   console.log(firstname);
 
@@ -88,7 +90,12 @@ const edit = ({ userr }) => {
       <div className="secondaryheading">
         <p className="secondaryheading__word">account details</p>
       </div>
-      {isUpdated && <p className="error">user details updated</p>}
+      {isUpdated && (
+        <p className="success">
+          <Loader />
+          user details updated
+        </p>
+      )}
       <form className="form" action="" onSubmit={handleSubmit}>
         <div className="form__group">
           <label className="form__label" htmlFor="firstname">
@@ -123,7 +130,7 @@ const edit = ({ userr }) => {
             className="form__input"
             type="text"
             name="phonenumber"
-            defaultValue={phonenumber ? userr.phonenumber : ''}
+            defaultValue={userr.phonenumber}
             onChange={handleChange}
           />
         </div>
@@ -138,7 +145,7 @@ const edit = ({ userr }) => {
           </select>
         </div>
 
-        <button className="form__button">
+        <button disabled={isDisabled} className="form__button">
           {isUpdating ? 'updating' : 'save'}
         </button>
       </form>
