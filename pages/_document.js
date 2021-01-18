85cc8a511a48e142d70fe67fb6518ab8401883e8
Guyra Anderson <burnx16@gmail.com>
2021-01-18 10:43:11 -0800

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,11 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
+            href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap"
+            rel="stylesheet"
+          ></link>
+
+          <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,900;1,300&display=swap"
             rel="stylesheet"
           />
